759b87eba84162a21cbd1b046443856235049a7e
telranAndroid <telran.asana@gmail.com>
2016-04-20 18:47:28 +0300

    08_Quantity_Picker_IncrementDecrement Fixed: increment-decrement functions;

--- app/src/main/java/com/yasam/justjava/MainActivity.java
+++ app/src/main/java/com/yasam/justjava/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
         int quantity = 2;
-        quantity = quantity + 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         int quantity = 3;
-        quantity = quantity - 1;
+        quantity = quantity + 1;
         display(quantity);
     }
 
